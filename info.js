f713b6d4d78930a9079671d453ac3916592befd2
joe2028 <96135184+joe2028@users.noreply.github.com>
2022-03-07 12:31:13 -0500

    main

---
+++ info.js
@@ -0,0 +1,6 @@
+//Created by bennett
+//all info can be seen on the github page
+
+var birthMonth = getElementById("birthMonthVar");
+var birthDay = getElementById("birthDayVar");
+var birthYear = getElementById("birthYearVar");
