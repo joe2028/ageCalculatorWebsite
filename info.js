0e227c059ef8b6f6a4197b7dab1f2359afb74c0c
joe2028 <96135184+joe2028@users.noreply.github.com>
2022-03-08 08:25:49 -0500

    main

--- info.js
+++ info.js
@@ -4,3 +4,7 @@
 var birthMonth = getElementById("birthMonthVar");
 var birthDay = getElementById("birthDayVar");
 var birthYear = getElementById("birthYearVar");
+
+var currentMonth = getElementById("birthMonthVar");
+var currentDay = getElementById("birthDayVar");
+var currentYear = getElementById("birthYearVar");
