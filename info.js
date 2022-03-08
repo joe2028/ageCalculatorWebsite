6a16503ecf1e0ecf62588cdfcdeff25def079a7c
joe2028 <96135184+joe2028@users.noreply.github.com>
2022-03-08 12:46:01 -0500

    updated js file

--- info.js
+++ info.js
@@ -1,10 +1,24 @@
 //Created by bennett
 //all info can be seen on the github page
 
-var birthMonth = getElementById("birthMonthVar");
-var birthDay = getElementById("birthDayVar");
-var birthYear = getElementById("birthYearVar");
+//Birth Year
+var birthMonth = document.getElementById("birthMonthVar");
+var birthDay = document.getElementById("birthDayVar");
+var birthYear = document.getElementById("birthYearVar");
 
-var currentMonth = getElementById("birthMonthVar");
-var currentDay = getElementById("birthDayVar");
-var currentYear = getElementById("birthYearVar");
+//Current Year
+var currentMonth = document.getElementById("birthMonthVar");
+var currentDay = document.getElementById("birthDayVar");
+var currentYear = document.getElementById("birthYearVar");
+
+//Other
+var currentSubmit = document.getElementById("currentSubmitVar");
+
+
+//Submit Button Function
+function submitFunc(){
+    const averageDaysAlive = ((currentYear - birthYear) * 365 + 
+    (currentMonth - birthMonth) * 30 + (currentDay - birthDay))
+    var submission = document.getElementById("submissionVar").textContent = "You have lived for " + averageDaysAlive + " days!";
+
+}
