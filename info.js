a939cfacd65b780400fbe9fd1caabbcb793c157d
joe2028 <96135184+joe2028@users.noreply.github.com>
2022-03-11 09:30:31 -0500

    FINALVERSION v.1

--- info.js
+++ info.js
@@ -2,24 +2,23 @@
 //all info can be seen on the github page
 
 //Birth Year
-var birthMonth = document.getElementById("birthMonthVar");
-var birthDay = document.getElementById("birthDayVar");
-var birthYear = document.getElementById("birthYearVar");
-
-//Current Year
-var currentMonth = document.getElementById("birthMonthVar");
-var currentDay = document.getElementById("birthDayVar");
-var currentYear = document.getElementById("birthYearVar");
-
-//Other
-var currentSubmit = document.getElementById("currentSubmitVar");
-var submission = document.getElementById("submissionVar")
-const averageDaysAlive = ((currentYear - birthYear) * 365 + 
-    (currentMonth - birthMonth) * 30 + (currentDay - birthDay))
 
     //Submit Button Function
-function submitFunction(){
-    
-    submission.innerHTML = "You have lived for ";
+function sun(){
+    var birthMonth = document.getElementById("birthMonthVar").value;
+    var birthDay = document.getElementById("birthDayVar").value;
+    var birthYear = document.getElementById("birthYearVar").value;
+
+    //Current Year
+    var currentMonth = document.getElementById("currentMonthVar").value;
+    var currentDay = document.getElementById("currentDayVar").value;
+    var currentYear = document.getElementById("currentYearVar").value;
+
+    //Other
+    var Submit = document.getElementById("currentSubmitVar");
+    const averageDaysAlive = ((currentYear - birthYear) * 365 + 
+    (currentMonth - birthMonth) * 30 + (currentDay - birthDay));
+    document.getElementById("submissionsVar").innerHTML = "You have lived for " + 
+    averageDaysAlive + " days.";
 
 }
